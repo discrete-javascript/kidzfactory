61e4013c221808c4634bd46438abcd2cecbb33ce
Rajesh <wraejesh@gmail.com>
2024-03-31 12:56:49 +0530

    feat(build:success): Android build

---
+++ android/app/src/main/kotlin/com/example/kidzfactory/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kidzfactory
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/web_view_stack.dart
@@ -0,0 +1,86 @@
+// Copyright 2022 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class WebViewStack extends StatefulWidget {
+  const WebViewStack({required this.controller, super.key});
+
+  final WebViewController controller;
+
+  @override
+  State<WebViewStack> createState() => _WebViewStackState();
+}
+
+class _WebViewStackState extends State<WebViewStack> {
+  var loadingPercentage = 0;
+
+  void _launchWhatsAppURL(String url) async {
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      throw 'Could not launch Whatsapp';
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (url) {
+            setState(() {
+              loadingPercentage = 0;
+            });
+          },
+          onProgress: (progress) {
+            setState(() {
+              loadingPercentage = progress;
+            });
+          },
+          onPageFinished: (url) {
+            setState(() {
+              loadingPercentage = 100;
+            });
+          },
+          onNavigationRequest: (navigation) {
+            final host = Uri.parse(navigation.url).host;
+            if (host.startsWith("https://wa.me/") ||
+                host.startsWith("https://api.whatsapp.com/send") ||
+                host.startsWith("whatsapp://send")) {
+              _launchWhatsAppURL(host); // Handling WhatsApp URL specifically
+              return NavigationDecision.prevent; // Prevent loading in WebView
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..addJavaScriptChannel(
+        'SnackBar',
+        onMessageReceived: (message) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text(message.message)));
+        },
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        WebViewWidget(
+          controller: widget.controller,
+        ),
+        if (loadingPercentage < 100)
+          LinearProgressIndicator(
+            value: loadingPercentage / 100.0,
+          ),
+      ],
+    );
+  }
+}
